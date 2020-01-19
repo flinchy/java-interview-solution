b666fd4f930802ec77ed831718a323da8beb1400
chisom <iwowochisom@gmail.com>
2020-01-19 16:58:14 +0100

    initial commit

---
+++ src/main/java/com/chisom/java_assessment_solution/JavaAssessmentSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.chisom.java_assessment_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssessmentSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAssessmentSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chisom/java_assessment_solution/JavaAssessmentSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chisom.java_assessment_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssessmentSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
