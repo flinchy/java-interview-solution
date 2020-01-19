4dda5be38a471073bf6e02a2b5d105b487ddab4f
chisom <iwowochisom@gmail.com>
2020-01-19 17:23:16 +0100

    feature(POJO): add country object to copy data from the external api

---
+++ src/main/java/com/chisom/java_assessment_solution/payload/binlist_response/Country.java
@@ -0,0 +1,14 @@
+package com.chisom.java_assessment_solution.payload.binlist_response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Country {
+
+    private String name;
+    private String emoji;
+}
