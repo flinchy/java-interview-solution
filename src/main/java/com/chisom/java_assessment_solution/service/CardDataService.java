c62c0e2738a1ed0747ac9da5a2ad09077c41062f
chisom <iwowochisom@gmail.com>
2020-01-19 18:08:09 +0100

    feature(consume-rest-api): add a method verify card, to consume rest api in card data service

--- src/main/java/com/chisom/java_assessment_solution/service/CardDataService.java
+++ src/main/java/com/chisom/java_assessment_solution/service/CardDataService.java
@@ -1,13 +1,20 @@
 package com.chisom.java_assessment_solution.service;
 
+import com.chisom.java_assessment_solution.payload.binlist_response.BinListResponse;
 import com.chisom.java_assessment_solution.payload.card_response.CardDataResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CardDataService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public CardDataResponse verifyCard(String cardNumber) {
 
-        return null;
+        BinListResponse binListResponse = restTemplate.getForObject("https://lookup.binlist.net/{cardNumber}", BinListResponse.class, cardNumber);
+        return TheCardResponse(binListResponse);
     }
 }
