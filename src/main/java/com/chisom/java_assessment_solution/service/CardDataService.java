b8b68b7c66c51c1a07f9487cce2800b39aea9e23
chisom <iwowochisom@gmail.com>
2020-01-19 18:27:29 +0100

    feature(card-data-response): add method to extract data from external api and return the desired response

--- src/main/java/com/chisom/java_assessment_solution/service/CardDataService.java
+++ src/main/java/com/chisom/java_assessment_solution/service/CardDataService.java
@@ -1,6 +1,7 @@
 package com.chisom.java_assessment_solution.service;
 
 import com.chisom.java_assessment_solution.payload.binlist_response.BinListResponse;
+import com.chisom.java_assessment_solution.payload.card_response.CardDataPayload;
 import com.chisom.java_assessment_solution.payload.card_response.CardDataResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,19 @@ public class CardDataService {
     public CardDataResponse verifyCard(String cardNumber) {
 
         BinListResponse binListResponse = restTemplate.getForObject("https://lookup.binlist.net/{cardNumber}", BinListResponse.class, cardNumber);
-        return TheCardResponse(binListResponse);
+        return TheCardDataResponse(binListResponse);
+    }
+
+    public CardDataResponse TheCardDataResponse(BinListResponse binListResponse) {
+        CardDataResponse cardDataResponse = new CardDataResponse();
+        CardDataPayload cardDataPayload = new CardDataPayload();
+
+        cardDataResponse.setSuccess(binListResponse.getNumber().isLuhn());
+        cardDataResponse.setPayload(cardDataPayload);
+        cardDataPayload.setScheme(binListResponse.getScheme());
+        cardDataPayload.setType(binListResponse.getType());
+        cardDataPayload.setBank(binListResponse.toString());
+
+        return cardDataResponse;
     }
 }
