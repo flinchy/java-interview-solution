c8bd4a468c75ef462a18c8d56c670e9cfa91188b
chisom <iwowochisom@gmail.com>
2020-01-19 17:59:38 +0100

    feature(rest-template-bean): add rest template for making external api call to bean

--- src/main/java/com/chisom/java_assessment_solution/JavaAssessmentSolutionApplication.java
+++ src/main/java/com/chisom/java_assessment_solution/JavaAssessmentSolutionApplication.java
@@ -2,12 +2,20 @@ package com.chisom.java_assessment_solution;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JavaAssessmentSolutionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaAssessmentSolutionApplication.class, args);
+	}
+
+	@Bean
+	RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
--- src/main/java/com/chisom/java_assessment_solution/service/CardDataService.java
+++ src/main/java/com/chisom/java_assessment_solution/service/CardDataService.java
@@ -1,8 +1,13 @@
 package com.chisom.java_assessment_solution.service;
 
+import com.chisom.java_assessment_solution.payload.card_response.CardDataResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CardDataService {
-    
+
+    public CardDataResponse verifyCard(String cardNumber) {
+
+        return null;
+    }
 }
