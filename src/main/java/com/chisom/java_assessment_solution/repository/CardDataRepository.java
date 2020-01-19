1b742fb5e3cd401c1c04c6265f26012328d03cbb
chisom <iwowochisom@gmail.com>
2020-01-20 00:39:53 +0100

    feature(query-database): write raw sql query to get card statistics

--- src/main/java/com/chisom/java_assessment_solution/repository/CardDataRepository.java
+++ src/main/java/com/chisom/java_assessment_solution/repository/CardDataRepository.java
@@ -1,7 +1,15 @@
 package com.chisom.java_assessment_solution.repository;
 
 import com.chisom.java_assessment_solution.model.CardData;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Map;
 
 public interface CardDataRepository extends JpaRepository<CardData, Long> {
+
+    @Query("SELECT card.cardNumber AS cardNumber, COUNT(card.cardNumber) AS count FROM CardData card GROUP BY card.cardNumber ORDER BY COUNT(card.cardNumber) DESC")
+    Page<Map<String, Object>> getPage(Pageable page);
 }
